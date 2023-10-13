5aba686586f435bb09cc5b51961d390112914c92
aisha-png <67974517+aisha-png@users.noreply.github.com>
2023-10-13 16:46:54 +0100

    go back button in customiser works

--- client/src/pages/Customiser.jsx
+++ client/src/pages/Customiser.jsx
@@ -11,8 +11,45 @@ import { fadeAnimation, slideAnimation } from '../config/motion';
 import { AIPicker, ColorPicker, CustomButton, FilePicker, Tab } from '../components';
 
 const Customiser = () => {
+  const snap = useSnapshot(state);
+
   return (
-    <div>Customiser</div>
+    <AnimatePresence>
+      {!snap.intro && (
+        <>
+          <motion.div
+            key="custom"
+            className='absolute top-0 left-0 z-10'
+            {...slideAnimation('left')}
+          >
+            <div className='flex items-center min-h-screen'>
+              <div className='editortabs-container tabs'>
+                {EditorTabs.map((tab) => (
+                  <Tab 
+                    key={tab.name}
+                    tab={tab}
+                    handleClick={() => {}}
+                  />
+                ))}
+              </div>
+            </div>
+          </motion.div>
+
+          <motion.div
+            className='absolute z-10 top-5 right-5'
+            {...fadeAnimation}
+          >
+            <CustomButton 
+              type='filled'
+              title="Go Bsck"
+              handleClick={() => state.intro = true}
+              customStyles='w-fit px-4 py-2.5 font-bold text-sm'
+            />
+          </motion.div>
+        </>
+      )}
+
+    </AnimatePresence>
   )
 }
 
