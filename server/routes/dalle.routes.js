bc76ca8454fcad3111380e481bebb36c60773a51
aisha-png <67974517+aisha-png@users.noreply.github.com>
2023-10-16 19:59:24 +0100

    changing openai import from v3 to v4

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -2,18 +2,16 @@
 
 import express from 'express';
 import * as dotenv from 'dotenv';
-// import { Configuration, OpenAIApi } from 'openai';
+import OpenAI from 'openai';
 
 
 dotenv.config();
 
 const router = express.Router();
 
-// const config = new Configuration({
-//     apiKey: process.env.OPENAI_API_KEY,
-// });
-
-// const openai = new OpenAIApi(config);
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY, 
+  });
 
 router.route('/').get((req, res) => {
     res.status(200).json({message: "Hello from DALL.E ROUTES"})
