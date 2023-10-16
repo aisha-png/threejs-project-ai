c69b84a45c9953091af9fdec5f860bb97cbe254d
aisha-png <67974517+aisha-png@users.noreply.github.com>
2023-10-16 17:09:30 +0100

    commented openai code

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -2,18 +2,18 @@
 
 import express from 'express';
 import * as dotenv from 'dotenv';
-import { Configuration, OpenAIApi } from 'openai';
+// import { Configuration, OpenAIApi } from 'openai';
 
 
 dotenv.config();
 
 const router = express.Router();
 
-const config = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-});
+// const config = new Configuration({
+//     apiKey: process.env.OPENAI_API_KEY,
+// });
 
-const openai = new OpenAIApi(config);
+// const openai = new OpenAIApi(config);
 
 router.route('/').get((req, res) => {
     res.status(200).json({message: "Hello from DALL.E ROUTES"})
