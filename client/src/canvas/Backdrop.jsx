676924fb4f039170fdb5c8255fd825ead923e91d
aisha-png <67974517+aisha-png@users.noreply.github.com>
2023-10-21 22:32:28 +0100

    made changes to backdrop shadows, background colour and using better method for images from openai

--- client/src/canvas/Backdrop.jsx
+++ client/src/canvas/Backdrop.jsx
@@ -21,14 +21,14 @@ const Backdrop = () => {
       <RandomizedLight 
         amount={4}
         radius={9}
-        intensity={0.55}
+        intensity={1}
         ambient={0.25}
         position={[5, 5, -10]}
       /> 
       <RandomizedLight 
         amount={4}
         radius={5}
-        intensity={0.25}
+        intensity={0.85}
         ambient={0.55}
         position={[-5, 5, -9]}
       /> 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -36,7 +36,7 @@ const Home = () => {
             {...headContentAnimation}
             className='flex flex-col gap-5'
           >
-            <p className='max-w-md font-normal text-gray-600
+            <p className='max-w-md font-normal text-gray-400
             text-base'>
               Create your unique and exclusive shirt with our brand-new 3D
               customisation too. <strong>Unleash your imagination</strong>{" "}
