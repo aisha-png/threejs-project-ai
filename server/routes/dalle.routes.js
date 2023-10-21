ac0533a44ac1fb8169e8298dc3ce4ed883cf861a
aisha-png <67974517+aisha-png@users.noreply.github.com>
2023-10-21 22:33:12 +0100

    commiting the entire project to git

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -4,10 +4,13 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import OpenAI from 'openai';
 
-
 dotenv.config();
 
 const router = express.Router();
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
 router.route('/').get((req, res) => {
   res.status(200).json({ message: "Hello from DALL.E ROUTES" });
@@ -17,61 +20,21 @@ router.route('/').post(async (req, res) => {
   try {
     const { prompt } = req.body;
 
-    const response = await OpenAI.Image.create({
+    const response = await openai.images.generate({ 
       prompt,
       n: 1,
       size: '1024x1024',
-      response_format: 'b64_json',
-      // apiKey: process.env.OPENAI_API_KEY,
+      response_format: 'url',
     });
 
-    // const image = response.data.choices[0].text;
-    const image = response.data.data[0].b64_json;
+    const imageUrl = response.data.url;
 
-    res.status(200).json({ photo: image });
+    res.status(200).json({ photo: imageUrl });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Something went wrong" });
   }
 });
 
-
-// dotenv.config();
-
-// const router = express.Router();
-
-// const openai = new OpenAI({
-//     apiKey: process.env.OPENAI_API_KEY, 
-// });
-// // const openai = () => {
-// //   OpenAI.configure({
-// //     apiKey: process.env.OPENAI_API_KEY,
-// //   });
-// // };
-//   // const openai = new OpenAI(config);
-
-// router.route('/').get((req, res) => {
-//     res.status(200).json({message: "Hello from DALL.E ROUTES"})
-// })
-
-
-// router.route('/').post(async (req, res) => {
-//   try {
-//     const {prompt} = req.body;
-
-//     const response = await openai.createImage({
-//       prompt,
-//       n: 1,
-//       size: '1024x1024',
-//       response_format: 'b64_json'
-//     });
-//     const image = response.data.data[0].b64_json;
-//     res.status(200).json({ photo: image});
-
-//   } catch (error) {
-//     console.error(error)
-//     res.status(500).json({ message: "Something went wrong"})
-//   }
-// })
 
 export default router;
