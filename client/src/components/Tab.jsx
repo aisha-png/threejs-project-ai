3a8bc1aa4a81d547c4e04256e29efbbcfae650b2
aisha-png <67974517+aisha-png@users.noreply.github.com>
2023-10-22 10:28:45 +0100

    fixed spelling issue and included activeStyles to tab-btn

--- client/src/components/Tab.jsx
+++ client/src/components/Tab.jsx
@@ -18,11 +18,12 @@ const Tab = ({tab, isFilterTab, isActiveTab, handleClick }) => {
       className={`tab-btn ${isFilterTab ? 'rounded-full glassmorphism' 
       : 'rounded-4'}`}
       onClick={handleClick}
+      style={activeStyles}
     >
       <img 
         src={tab.icon}
         alt={tab.name}
-        className={`${isFilterTab ? 'w-2/3 h-2/3' : 'w-11/12 h-11/12 bject-contain'}`}
+        className={`${isFilterTab ? 'w-2/3 h-2/3' : 'w-11/12 h-11/12 object-contain'}`}
         />
     </div>
   )
